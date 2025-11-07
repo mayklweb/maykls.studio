21dc247bcdc9e80197bf628ca14e22098f0b1969
mayklweb <muhammadevvv@gmail.com>
2025-11-07 23:06:51 +0500

    fixed

--- app/provider/index.tsx
+++ app/provider/index.tsx
@@ -3,12 +3,19 @@
 import { ReactNode, useEffect } from "react";
 import Lenis from "@studio-freight/lenis";
 
-export default function Provider({ children }) {
+export default function Provider({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
   useEffect(() => {
-    const lenis = new Lenis({ duration: 1.2, smooth: true });
-    window.lenis = lenis;
+    const lenis = new Lenis({
+      lerp: 0.5, // scroll silliqligini boshqaradi (0–1 oralig‘ida)
+      wheelMultiplier: 1, // g‘ildirak tezligini boshqaradi
+      touchMultiplier: 1.5, // mobil uchun tezlik
+      infinite: false, // infinite scroll yo‘q bo‘lsa false
+    });
+    (window as any).lenis = lenis;
 
-    function raf(time) {
+    function raf(time: number) {
       lenis.raf(time);
       requestAnimationFrame(raf);
     }
